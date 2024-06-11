b3bcd03b5040c25c40575777a05ad721208c670c
Rodolfo Osorio V. <rodolfoeduardoov@gmail.com>
2024-06-11 00:29:33 -0400

    Castillo principal creado
    Tiene hp y una funcion para recibir daño

---
+++ scripts/castle.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+@export var vidaMaxima = 100
+var vida: float
+
+func _ready():
+	vida = vidaMaxima
+	
+func recibirDaño(daño):
+	vida -= daño
+	if vida <= 0:
+		self.queue_free()
+		"se acaba el juego"	
+	
