b6b6c892b40dc1f0b94c268db100c65dc9b5ff46
Rodolfo Osorio V. <rodolfoeduardoov@gmail.com>
2024-06-14 18:40:19 -0400

    spawn funciona

--- scripts/pathSpawner.gd
+++ scripts/pathSpawner.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-@export var spawns: Array[Spawn_info] = [] 
-@onready var player = get_tree().get_first_node_in_group("castle")
+@export var spawns: Array[Spawn_info] = []
 
 var enemySpawns
 var newEnemy
