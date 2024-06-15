ec5b96aaa558da0e51958622e8fdf09fcbc9e59b
David López <newperax@gmail.com>
2024-06-15 12:38:28 -0400

    coins

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -6,3 +6,10 @@ func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)	
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
+		
+
+func _physics_process(delta):
+	Input.is_key_pressed(KEY_ENTER)
+
+func _exit_tree():
+	Global.coins += 10
