8ce0a68de112655d8f735f65438e9d731214b544
ividal <inti.vidal@usm.cl>
2024-06-15 15:41:01 -0400

    Changed bullet and enemy health

--- scripts/castle.gd
+++ scripts/castle.gd
@@ -1,19 +1,17 @@
 extends Area2D
 
-@export var vidaMaxima = 100
+@export var vidaMaxima = 10
 var target : Node2D = null
 var vida: float
 
 func _ready():
 	vida = vidaMaxima
-	
+
 func recibirDaño(daño):
 	vida -= daño
 	if vida <= 0:
+		print("se acaba el juego")
 		self.queue_free()
-		"se acaba el juego"	
-
-
 
 func _on_body_entered(body):
 	if body.is_in_group("targets"):
