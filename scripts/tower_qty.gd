a5479b3f279a626b94ab0e9d7abbf35c5fa41b15
David López <newperax@gmail.com>
2024-06-15 17:37:15 -0400

    tower palacement fixed

--- scripts/tower_qty.gd
+++ scripts/tower_qty.gd
@@ -3,12 +3,12 @@ extends Label
 @export var scene:Node2D
 
 var tower = preload("res://scenes/Tower.tscn")
-var towertoplace = tower.instantiate()
 
 func _process(delta):
 	self.text = str(Global.tower_qty)
 
 func _on_button_pressed():
+	var towertoplace = tower.instantiate()
 	if Global.tower_qty > 0:
 		Global.tower_qty -= 1
 		scene.add_child(towertoplace)
