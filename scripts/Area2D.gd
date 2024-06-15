be7368753dd3e28ba9cb21ab1822af5a385f51e4
Rodolfo Osorio V. <110223941+rBaku@users.noreply.github.com>
2024-06-15 12:33:26 -0400

    se agrego maquina y se cambio el script de bullet y castillo

--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -1,9 +1,17 @@
 extends Area2D
 
 @export var speed : float = 400.0
+var target : Node2D = null
 var velocity:Vector2
+
 func _process(delta):
 	position += velocity * delta
 
 func set_velocity(direction):
 	velocity = direction
+
+
+func _on_body_entered(body):
+	if body.is_in_group("targets"):
+		self.queue_free()
+
