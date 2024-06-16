9ae392f98b7bce288c0603d6f8434adb441df0d4
Rodolfo Osorio V. <rodolfoeduardoov@gmail.com>
2024-06-16 13:33:29 -0400

    enemigos

---
+++ scripts/boss.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var speed: float = 200
+var health:= 100000000
+
+func _process(delta):
+	get_parent().set_progress(get_parent().get_progress() + speed*delta)	
+	if get_parent().get_progress_ratio() == 1:
+		queue_free()
+
+func _physics_process(delta):
+	if Input.is_key_pressed(KEY_ENTER):
+		queue_free()
